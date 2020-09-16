82b6aa682591f4c8879b6e50c5ebc9cd99afd843
Lucas Vogelsang <l@lucasvo.com>
2020-09-16 08:47:07 +0000

    WIP: test compiles with both Tinlake and Maker initiated

--- src/integration.t.sol
+++ src/integration.t.sol
@@ -1,34 +1,54 @@
 pragma solidity ^0.5.12;
 
 import "ds-test/test.sol";
-import "dss-deploy/DssDeploy.t.base.sol";
+import {DssDeployTestBase, DSValue} from "dss-deploy/DssDeploy.t.base.sol";
+import "dss-add-ilk-spell/DssAddIlkSpell.sol";
 
-import "tinlake-maker-lib/join.sol";
-import "tinlake-maker-lib/flip.sol";
+import "tinlake/test/system/base_system.sol";
+
+import {TinlakeJoin} from "tinlake-maker-lib/join.sol";
+import {TinlakeFlipper} from "tinlake-maker-lib/flip.sol";
 
 import {PipLike} from "dss/spot.sol";
 
-contract DssAddIlkSpellTest is DssDeployTestBase {
+contract DssAddIlkSpellTest is DssDeployTestBase, BaseSystemTest {
+    Hevm public hevm;
+
     DssAddIlkSpell spell;
 
-    bytes32 constant ilk = "NCT"; // New Collateral Type
-    DSToken      nct;
-    TinlakeJoin  nctJoin;
-    Flipper      nctFlip;
-    DSValue      nctPip;
+    bytes32 constant ilk = "TEST-DROP"; // New Collateral Type
+    TinlakeJoin     dropJoin;
+    TinlakeFlipper  dropFlip;
+    DSValue         dropPip;
 
     function setUp() public {
-        super.setUp();
+        DssDeployTestBase.setUp();
         deploy();
 
-        nct = new DSToken(ilk);
-        nct.mint(1 ether);
-        nctJoin = new GemJoin(address(vat), ilk, address(nct));
-        nctPip = new DSValue();
-        nctPip.poke(bytes32(uint(300 ether)));
-        nctFlip = flipFab.newFlip(address(vat), ilk);
-        nctFlip.rely(address(pause.proxy()));
-        nctFlip.deny(address(this));
+        hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+        hevm.warp(1234567);
+        deployLenderMockBorrower(address(this));
+        createInvestorUser();
+
+        uint seniorAmount =  82 ether;
+        uint juniorAmount = 18 ether;
+        seniorSupply(seniorAmount, seniorInvestor);
+        juniorSupply(juniorAmount);
+        hevm.warp(now + 1 days);
+
+        coordinator.closeEpoch();
+        // no submission required
+        // submission was valid
+        assertTrue(coordinator.submissionPeriod() == false);
+        // inital token price is ONE
+        (uint payoutCurrencyAmount, uint payoutTokenAmount, uint remainingSupplyCurrency,  uint remainingRedeemToken) = seniorInvestor.disburse();
+
+        dropJoin = new TinlakeJoin(address(vat), ilk, address(seniorToken));
+        dropPip = new DSValue();
+        dropPip.poke(bytes32(uint(300 ether)));
+        dropFlip = new TinlakeFlipper(address(vat), ilk);
+        dropFlip.rely(address(pause.proxy()));
+        dropFlip.deny(address(this));
 
         spell = new DssAddIlkSpell(
             ilk,
@@ -39,9 +59,9 @@ contract DssAddIlkSpellTest is DssDeployTestBase {
                 address(jug),
                 address(spotter),
                 address(end),
-                address(nctJoin),
-                address(nctPip),
-                address(nctFlip)
+                address(dropJoin),
+                address(dropPip),
+                address(dropFlip)
             ],
             [
                 10000 * 10 ** 45, // line
@@ -57,27 +77,43 @@ contract DssAddIlkSpellTest is DssDeployTestBase {
         spell.schedule();
         spell.cast();
 
-        nct.approve(address(nctJoin), 1 ether);
+        seniorToken.approve(address(dropJoin), uint(-1));
+    }
+
+    function seniorSupply(uint currencyAmount, Investor investor) public {
+        seniorMemberlist.updateMember(seniorInvestor_, safeAdd(now, 8 days));
+        currency.mint(address(seniorInvestor), currencyAmount);
+        investor.supplyOrder(currencyAmount);
+        (,uint supplyAmount, ) = seniorTranche.users(address(investor));
+        assertEq(supplyAmount, currencyAmount);
+    }
+
+    function juniorSupply(uint currencyAmount) public {
+        juniorMemberlist.updateMember(juniorInvestor_, safeAdd(now, 8 days));
+        currency.mint(address(juniorInvestor), currencyAmount);
+        juniorInvestor.supplyOrder(currencyAmount);
+        (,uint supplyAmount, ) = juniorTranche.users(juniorInvestor_);
+        assertEq(supplyAmount, currencyAmount);
     }
 
     function testVariables() public {
         (,,,uint line,) = vat.ilks(ilk);
         assertEq(line, uint(10000 * 10 ** 45));
         (PipLike pip, uint mat) = spotter.ilks(ilk);
-        assertEq(address(pip), address(nctPip));
+        assertEq(address(pip), address(dropPip));
         assertEq(mat, uint(1500000000 ether));
         (uint tax,) = jug.ilks(ilk);
         assertEq(tax, uint(1.05 * 10 ** 27));
         (address flip, uint chop, uint lump) = cat.ilks(ilk);
-        assertEq(flip, address(nctFlip));
+        assertEq(flip, address(dropFlip));
         assertEq(chop, ONE);
         assertEq(lump, uint(10000 ether));
-        assertEq(vat.wards(address(nctJoin)), 1);
+        assertEq(vat.wards(address(dropJoin)), 1);
     }
 
     function testFrob() public {
         assertEq(dai.balanceOf(address(this)), 0);
-        nctJoin.join(address(this), 1 ether);
+        dropJoin.join(address(this), 1 ether);
 
         vat.frob(ilk, address(this), address(this), address(this), 1 ether, 100 ether);
 
@@ -89,13 +125,13 @@ contract DssAddIlkSpellTest is DssDeployTestBase {
     function testFlip() public {
         this.file(address(cat), ilk, "lump", 1 ether); // 1 unit of collateral per batch
         this.file(address(cat), ilk, "chop", ONE);
-        nctJoin.join(address(this), 1 ether);
+        dropJoin.join(address(this), 1 ether);
         vat.frob(ilk, address(this), address(this), address(this), 1 ether, 200 ether); // Maximun DAI generated
-        nctPip.poke(bytes32(uint(300 ether - 1))); // Decrease price in 1 wei
+        dropPip.poke(bytes32(uint(300 ether - 1))); // Decrease price in 1 wei
         spotter.poke(ilk);
-        assertEq(vat.gem(ilk, address(nctFlip)), 0);
+        assertEq(vat.gem(ilk, address(dropFlip)), 0);
         uint batchId = cat.bite(ilk, address(this));
-        assertEq(vat.gem(ilk, address(nctFlip)), 1 ether);
+        assertEq(vat.gem(ilk, address(dropFlip)), 1 ether);
 
         address(user1).transfer(10 ether);
         user1.doEthJoin(address(weth), address(ethJoin), address(user1), 10 ether);
@@ -105,19 +141,12 @@ contract DssAddIlkSpellTest is DssDeployTestBase {
         user2.doEthJoin(address(weth), address(ethJoin), address(user2), 10 ether);
         user2.doFrob(address(vat), "ETH", address(user2), address(user2), address(user2), 10 ether, 1000 ether);
 
-        user1.doHope(address(vat), address(nctFlip));
-        user2.doHope(address(vat), address(nctFlip));
+        user1.doHope(address(vat), address(dropFlip));
+        user2.doHope(address(vat), address(dropFlip));
 
-        user1.doTend(address(nctFlip), batchId, 1 ether, rad(100 ether));
-        user2.doTend(address(nctFlip), batchId, 1 ether, rad(140 ether));
-        user1.doTend(address(nctFlip), batchId, 1 ether, rad(180 ether));
-        user2.doTend(address(nctFlip), batchId, 1 ether, rad(200 ether));
-
-        user1.doDent(address(nctFlip), batchId, 0.8 ether, rad(200 ether));
-        user2.doDent(address(nctFlip), batchId, 0.7 ether, rad(200 ether));
-        hevm.warp(nctFlip.ttl() - 1);
-        user1.doDent(address(nctFlip), batchId, 0.6 ether, rad(200 ether));
-        hevm.warp(now + nctFlip.ttl() + 1);
-        user1.doDeal(address(nctFlip), batchId);
+        user1.doTend(address(dropFlip), batchId, 1 ether, rad(100 ether));
+        user2.doTend(address(dropFlip), batchId, 1 ether, rad(140 ether));
+        user1.doTend(address(dropFlip), batchId, 1 ether, rad(180 ether));
+        user2.doTend(address(dropFlip), batchId, 1 ether, rad(200 ether));
     }
 }
